ffbd3dce8fe9740e8dbce9f5fcbfb97e439a4104
Tran Tue Nhi <tuenhi2001@gmail.com>
2018-07-11 16:58:09 +0700

    initial comment

--- Session06/Assignment06/perfectnumber.py
+++ Session06/Assignment06/perfectnumber.py
@@ -3,7 +3,8 @@ result = 0
 for i in range (1, num):
     if (num%i)==0:
         result = result + i
-if result == num:
-    print(num, "is a perfect number!")
-else:
+if result != num or result == 0:
     print(num, "is NOT a perfect number")
+elif result == num:
+    print(num, "is a perfect number!")
+
